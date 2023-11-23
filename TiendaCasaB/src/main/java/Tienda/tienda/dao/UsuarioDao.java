7f96349abcad64b4c58acbdc22ce6c016d9b10ca
celeste9192 <108299741+celeste9192@users.noreply.github.com>
2023-11-22 23:15:48 -0600

    segunda parte

--- TiendaCasaB/src/main/java/Tienda/tienda/dao/UsuarioDao.java
+++ TiendaCasaB/src/main/java/Tienda/tienda/dao/UsuarioDao.java
@@ -7,6 +7,12 @@ package Tienda.tienda.dao;
 import Tienda.tienda.domain.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UsuarioDao extends JpaRepository<Usuario, Long> {    
+public interface UsuarioDao extends JpaRepository<Usuario, Long> {
     Usuario findByUsername(String username);
+    
+    Usuario findByUsernameAndPassword(String username, String Password);
+
+    Usuario findByUsernameOrCorreo(String username, String correo);
+
+    boolean existsByUsernameOrCorreo(String username, String correo);
 }
